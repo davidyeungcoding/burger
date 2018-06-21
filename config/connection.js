95a812c01a487a696c602ea3760bc8a1a9923e9a
yeungpirate <yeungpirate@gmail.com>
2018-06-21 12:42:55 -0700

    database connected with heroku

--- config/connection.js
+++ config/connection.js
@@ -1,6 +1,19 @@
 var mysql = require('mysql');
+require('dotenv').config();
 
-var connection = mysql.createConnection(process.env.JAWSDB_URL || process.env.local);
+if (process.env.JAWSDB_URL) {
+    connection = mysql.createConnection(process.env.JAWSDB_URL);
+}
+else {
+    connection = mysql.createConnection({
+        host: 'localhost',
+        user: 'root',
+        password: 'hacktheplanet',
+        database: 'todoagain_db'
+    });
+};
+
+// var connection = mysql.createConnection(process.env.JAWSDB_URL || process.env.test);
 
 connection.connect(function(err) {
     if (err) throw err;
