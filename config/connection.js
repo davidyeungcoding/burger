cee89dc7a266f863f730dc257fe50345370aa35a
yeungpirate <yeungpirate@gmail.com>
2018-06-21 10:02:09 -0700

    setting up env file

--- config/connection.js
+++ config/connection.js
@@ -1,14 +1,6 @@
 var mysql = require('mysql');
 
-var local = {
-    host: 'localhost',
-    user: 'root',
-    port: 3306,
-    password: '',
-    database: 'burgers_db'
-}
-
-var connection = mysql.createConnection(process.env.JAWSDB_URL || local);
+var connection = mysql.createConnection(process.env.JAWSDB_URL || process.env.local);
 
 connection.connect(function(err) {
     if (err) throw err;
